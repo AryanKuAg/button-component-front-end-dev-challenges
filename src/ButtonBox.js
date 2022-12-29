f967cfdf2b2a4207969c639bf76c99628b119f07
AryanKuAg <alemantrixaryan@gmail.com>
2022-12-30 03:18:04 +0530

    dynamically contents are generating

---
+++ src/ButtonBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ButtonBox(props) {
+    
+  return (
+    <div className='min-w-[250px] mr-8 mb-12 '>
+        <p className='mb-2 text-gray-600 font-medium'>{props.attribute}</p>
+        <button style={{...props, }} className="font-medium ">{props.text}</button>
+    </div>
+  )
+}
+
+export default ButtonBox
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import ButtonBox from './ButtonBox';
+import buttonData from './data.json'
 
 function Main() {
+    const data = buttonData;
+
   return (
-    <div className='w-full bg-red-300'>Main</div>
+    <main className='w-full  pl-20 pt-16 h-screen overflow-y-scroll'>
+        <h1 className='text-4xl font-bold text-gray-600 mb-16'>{data.header}</h1>
+        <div>
+            {data.rows.map(row => <div className='flex'>{row.columns.map(col => <ButtonBox {...col} />)}</div> )}
+        </div>
+    </main>
   )
 }
 
