cf4ac720fdef5dc86b37693be2e5765c9b790ebb
AryanKuAg <alemantrixaryan@gmail.com>
2022-12-30 03:37:30 +0530

    changes button sizes

--- src/ButtonBox.js
+++ src/ButtonBox.js
@@ -4,7 +4,7 @@ function ButtonBox(props) {
     
   return (
     <div className='min-w-[250px] mr-8 mb-12 '>
-        <p className='mb-2 text-gray-600 font-medium'>{props.attribute}</p>
+        <p className='mb-2 text-gray-600 font-medium' style={!props.attribute? {marginBottom: '32px'}: {}}>{props.attribute}</p>
         <button style={{...props, }} className="font-medium ">{props.text}</button>
     </div>
   )
